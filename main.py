18c8f48a21672745b1110abd29a3aaca8bd6e52a
Dj4t9n <pandeyjatin47@gmail.com>
2021-06-19 17:03:32 +0530

    STATUS:WORKING

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ import numpy as np
 import argparse
 import time
 import cv2
+import os
 
-ap = argparse.ArgumentParser()
+ap:any = argparse.ArgumentParser()
 ap.add_argument(
-        "-ip", "--image-path", type=str,
-	help="path to input image"
+        "-ip", "--image", type=str,
+	help="path to input work_image"
     )
 ap.add_argument(
         "-EAST", "--EAST", type=str,
@@ -15,14 +16,42 @@ ap.add_argument(
     )
 ap.add_argument(
         "-c", "--min-confidence", type=float, default=0.5,
-	help="minimum probability required to inspect a region"
+	help="minimum probability required to inspect a region(defalut=0.5)"
     )
 ap.add_argument(
-        "-w", "--width", type=int, default=320,
-	help="image width (multiple of 32, default=320)"
+        "-W", "--width", type=int, default=320,
+	help="work_image width (multiple of 32, default=320)"
     )
 ap.add_argument(
-        "-h", "--height", type=int, default=320,
-	help="image height (multiple of 32, default=320)"
+        "-H", "--height", type=int, default=320,
+	help="work_image height (multiple of 32, default=320)"
     )
-args = vars(ap.parse_args())
+WARNING = """
+The EAST text requires that your input work_image dimensions be multiples of 32,
+while choosing your width (--width) and height (--height),
+make sure they are multiples of 32
+"""
+
+args:dict = vars(ap.parse_args())
+
+
+work_image: np.array = cv2.imread(os.path.abspath(args["image"]))
+orig: np.array = work_image.copy()
+
+print(orig)
+
+IMG_H: int = work_image.shape[0]
+IMG_W: int = work_image.shape[1]
+
+
+newW: int = args["width"]
+newH: int = args["height"]
+
+if (newW + newH)%32 != 0:
+    print("please read instructions")
+    exit(1)
+
+rW = IMG_W / int(newW)
+rH = IMG_H / int(newH)
+work_image = cv2.resize(work_image, (newW, newH))
+(H, W) = work_image.shape[:2]
