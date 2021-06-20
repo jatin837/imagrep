ec0d7ef71a0874210fd2c787a55f178225c89e4c
Dj4t9n <pandeyjatin47@gmail.com>
2021-06-20 17:56:46 +0530

    remove arg parser

--- main.py
+++ main.py
@@ -1,60 +1,27 @@
 from imutils.object_detection import non_max_suppression
 import numpy as np
-import argparse
 import time
 import cv2
 import os
 
-ap:any = argparse.ArgumentParser()
-ap.add_argument(
-        "-ip", "--image", type=str,
-	help="path to input work_image"
-    )
-ap.add_argument(
-        "-EAST", "--EAST", type=str,
-	help="path to input EAST text detector"
-    )
-ap.add_argument(
-        "-c", "--min-confidence", type=float, default=0.5,
-	help="minimum probability required to inspect a region(defalut=0.5)"
-    )
-ap.add_argument(
-        "-W", "--width", type=int, default=320,
-	help="work_image width (multiple of 32, default=320)"
-    )
-ap.add_argument(
-        "-H", "--height", type=int, default=320,
-	help="work_image height (multiple of 32, default=320)"
-    )
+
+
+image_path = "https://i2.wp.com/syncedreview.com/wp-content/uploads/2020/04/image-82.png"
+model_path = os.path.abspath("./frozen_east_text_detection.pb")
+min_confidence = 0.5
+
 WARNING = """
 The EAST text requires that your input work_image dimensions be multiples of 32,
 while choosing your width (--width) and height (--height),
 make sure they are multiples of 32
 """
 
-args:dict = vars(ap.parse_args())
-
-
-work_image: np.array = cv2.imread(os.path.abspath(args["image"]))
+work_image: np.array = cv2.imread(image_path)
 orig: np.array = work_image.copy()
-
-print(orig)
 
 IMG_H: int = work_image.shape[0]
 IMG_W: int = work_image.shape[1]
 
-
-newW: int = args["width"]
-newH: int = args["height"]
-
-if (newW + newH)%32 != 0:
-    print("please read instructions")
-    exit(1)
-
-rW = IMG_W / int(newW)
-rH = IMG_H / int(newH)
-work_image = cv2.resize(work_image, (newW, newH))
-(H, W) = work_image.shape[:2]
 
 # define the two output layer names for the EAST detector model that
 # we are interested -- the first is the output probabilities and the
@@ -63,25 +30,29 @@ work_image = cv2.resize(work_image, (newW, newH))
 layer_names = [
 	"feature_fusion/Conv_7/Sigmoid",
 	"feature_fusion/concat_3"
-    ]
-# load the pre-trained EAST text detector
-print("[INFO] loading EAST text detector...")
-net = cv2.dnn.readNet(args["EAST"])
+]
+
+net = cv2.dnn.readNet(model_path)
 
 # construct a blob from the image and then perform a forward pass of
 # the model to obtain the two output layer sets
 
+scale_factor: float = 1.0
+size: tuple(int, int) = (IMG_W, IMG_H)
+scaler_mean: tuple(float, float, float) = (123.68, 116.78, 103.94)
+
 blob = cv2.dnn.blobFromImage(
         work_image,
-        1.0, 
-        (W, H),
-        (123.68, 116.78, 103.94), 
-        swapRB=True, crop=False
-    )
+        scale_factor, 
+        size,
+        scaler_mean, 
+        swapRB=True, 
+        crop=False
+)
 
 start = time.time()
 net.setInput(blob)
-(scores, geometry) = net.forward(layerNames)
+(scores, geometry) = net.forward(layer_names)
 end = time.time()
 # show timing information on text prediction
 print("[INFO] text detection took {:.6f} seconds".format(end - start))
--- ocr.py
+++ ocr.py
@@ -1,28 +1,21 @@
 from PIL import Image
 import pytesseract
-import argparse
 import cv2
 import os
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True,
-	help="path to input image to be OCR'd")
-ap.add_argument("-p", "--preprocess", type=str, default="thresh",
-	help="type of preprocessing to be done")
-args = vars(ap.parse_args())
 
-
+image_path = os.path.abspath("") # image path goes here
+PREPROCESS = "thresh" # or "blur"
 # load the example image and convert it to grayscale
-image = cv2.imread(args["image"])
+image = cv2.imread(image_path)
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 # check to see if we should apply thresholding to preprocess the
 # image
-if args["preprocess"] == "thresh":
+if PREPROCESS == "thresh":
 	gray = cv2.threshold(gray, 0, 255,
 		cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
 # make a check to see if median blurring should be done to remove
 # noise
-elif args["preprocess"] == "blur":
+elif PREPROCESS == "blur":
 	gray = cv2.medianBlur(gray, 3)
 # write the grayscale image to disk as a temporary file so we can
 # apply OCR to it
