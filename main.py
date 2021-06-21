3ebaaeebd74fcb7ee02c38b1c30bf61a478c3266
dj4t9n <pandeyjatin47@gmail.com>
2021-06-21 18:22:12 +0530

    STATUS:INSPECTING TODO:   - create a class `Word` and for each word, store those property that     you can infer

--- main.py
+++ main.py
@@ -6,8 +6,17 @@ if __name__ == "__main__":
     img = cv2.imread('test.png')
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     result = pytesseract.image_to_data(img, lang='eng', nice=0, output_type=pytesseract.Output.DICT)
-    with open("out.json", 'w') as f:
-        json.dump(result, f, indent = 2)
-    print(result)
     print("*"*10)
-    print(result.keys())
+    
+   # for key in result.keys():
+   #     print(f"length for {key} --> {len(result[key])}")
+   
+    keys = result.keys()
+    print(keys)
+    for i, word in enumerate(result['text']):
+        
+        p = (p := f'{word} has')
+        for key in keys:
+           p = p + f" {key} = {result[key][i]}"
+
+        print(p)
