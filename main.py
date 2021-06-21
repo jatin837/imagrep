6246a19ca75228aa83bd553472fe0613fb411636
dj4t9n <pandeyjatin47@gmail.com>
2021-06-21 17:41:49 +0530

    STATUS:INSPECTING
    inspecting the form of data API provides

--- main.py
+++ main.py
@@ -8,4 +8,6 @@ if __name__ == "__main__":
     result = pytesseract.image_to_data(img, lang='eng', nice=0, output_type=pytesseract.Output.DICT)
     with open("out.json", 'w') as f:
         json.dump(result, f, indent = 2)
-    type(result)
+    print(result)
+    print("*"*10)
+    print(result.keys())
--- main2.py
+++ main2.py
@@ -1,17 +1,22 @@
 import cv2
 import pytesseract
 
+class letter(object):
+    def __init__(this, 
+
 img = cv2.imread('test.png')
 
 h, w, c = img.shape
 boxes = pytesseract.image_to_boxes(img) 
+
+    
 # This is the method to get boxes around text in image
 # splitline is used for string
 # does that means boxes is a string?
-for b in boxes.splitlines():
-    b = b.split(' ')
-    img = cv2.rectangle(img, (int(b[1]), h - int(b[2])), (int(b[3]), h - int(b[4])), (0, 255, 0), 2)
+#for b in boxes.splitlines():
+#    b = b.split(' ')
+#    img = cv2.rectangle(img, (int(b[1]), h - int(b[2])), (int(b[3]), h - int(b[4])), (0, 255, 0), 2)
 
-print(boxes)
+print(type(boxes))
 #cv2.imshow('img', img)
 #cv2.waitKey(0) 
