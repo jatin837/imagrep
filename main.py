25f4f8b0589ff819686f5fe55188908d270f7dbe
dj4t9n <pandeyjatin47@gmail.com>
2021-06-22 09:33:58 +0530

    add + operator for text

--- main.py
+++ main.py
@@ -19,6 +19,9 @@ class Text(object):
         return ' '.join([f"{word.content}" for word in self.words])
     def __len__(self):
         return len(self.words)
+    def __add__(self, w):
+        self.words.append(w)
+
 
 if __name__ == "__main__":
     img = cv2.imread('test.png')
@@ -28,14 +31,14 @@ if __name__ == "__main__":
 
     text = Text()
     for i in range(len(result['text'])):
-        text.words.append(Word(
+        text + Word(
             result['left'][i],
             result['top'][i],
             result['width'][i],
             result['height'][i],
             result['text'][i]
 
-            ))
+            )
 
    # for key in result.keys():
    #     print(f"length for {key} --> {len(result[key])}")
@@ -55,7 +58,7 @@ if __name__ == "__main__":
     n_boxes = len(text)
     for i in range(n_boxes):
         if int(result['conf'][i]) > 60:
-                (x, y, w, h) = (result['left'][i], result['top'][i], result['width'][i], result['height'][i])
-                img = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            (x, y, w, h) = (result['left'][i], result['top'][i], result['width'][i], result['height'][i])
+            img = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
     cv2.imshow('img', img)
     cv2.waitKey(0)
