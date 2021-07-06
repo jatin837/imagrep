8797cd3b89dc97096ea491b92c5b0ccbec97f32c
bobbyjones <jatinpandey6588@gmail.com>
2021-07-06 12:07:54 +0530

    removing previous implementation of text and word classes

--- libs.py
+++ libs.py
@@ -1,18 +1,16 @@
 class Word(object):
-    def __init__(self, content, x, y, width, height):
-        self.content = content
+    def __init__(self, x:int, y:int, l:int, w:int, content:str):
         self.x = x
         self.y = y
-        self.width = width
-        self.height = height
-
+        self.l = l
+        self.w = w
+        self.content = content
     def __repr__(self):
-        return f"{self.content} at ({self.x}, {self.y}) with width = {self.width} and height = {self.height}"
+        return f"{self.content} at ({self.x}, {self,y}), dim = ({self.l}X{self.w})"
 
 class Text(object):
-    def __init__(self, words):
+    def __init__(self, words:list = []):
         self.words = words
-    
-    def to_text(self):
-        return ' '.join([word.content for word in self.words])
+    def __repr__(self):
+        return ' '.join([f"{word.content}" for word in self.words])
 
