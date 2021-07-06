50e81efab2618caf80eb7dd1862f04b615a799be
dj4t9n <pandeyjatin47@gmail.com>
2021-07-06 15:13:04 +0530

    debugging

--- main.py
+++ main.py
@@ -32,28 +32,17 @@ if __name__ == "__main__":
             result['width'][i],
             result['height'][i],
             result['text'][i]
+            ))
 
-        ))
-
-   # for key in result.keys():
-   #     print(f"length for {key} --> {len(result[key])}")
-   
-   #keys = result.keys()
-   #print(keys)
-   #for i, word in enumerate(result['text']):
-   #
-   #    p = (p := f'{word} has')
-   #    for key in keys:
-   #       p = p + f" {key} = {result[key][i]}"
-
-   #    print(p)
     print(text)
 
 
     n_boxes = len(result['text'])
     for i in range(n_boxes):
         if int(result['conf'][i]) > 60:
-                (x, y, w, h) = (result['left'][i], result['top'][i], result['width'][i], result['height'][i])
-                img = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
-    cv2.imshow('img', img)
-    cv2.waitKey(0)
+            (x, y, w, h) = (result['left'][i], result['top'][i], result['width'][i], result['height'][i])
+            img = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            cv2.imwrite(f"{i}_out.png", img)
+
+   #cv2.imshow('img', img)
+   #cv2.waitKey(0)
